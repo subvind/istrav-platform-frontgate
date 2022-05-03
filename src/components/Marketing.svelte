5da1b7dbe179bf573f5d642484b878492d3fa17a
istrav <travis.burandt@gmail.com>
2022-05-03 12:54:04 -0500

    searchable

--- src/components/Marketing.svelte
+++ src/components/Marketing.svelte
@@ -119,7 +119,7 @@
         </div>
       </div>
     </div>
-    <p class="detail">(notice: Unlike other social media platforms we are not here to take over and the world. We are simply here to provide Software as a Service. That's why we gave ourselves a very specific name "/community_folder/" rather than some abstract idea like "twitter" or "meta"; because we are in the business of code rather than people. That way members may fork us and run their own websites with their own rules.)</p>
+    <p class="detail">(notice: Unlike other social media platforms we are not here to take over and the world. We are simply here to provide Software as a Service. That's why we gave ourselves a very specific searchable name "/community_folder/" rather than some abstract idea like "twitter" or "meta"; because we are in the business of code rather than people. That way members may fork us and run their own websites with their own rules.)</p>
   </div>
   <br />
   <br />
