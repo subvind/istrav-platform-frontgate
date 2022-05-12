65157825e0136a8997d3012c7fd3ee1614d398eb
istrav <travis.burandt@gmail.com>
2022-05-12 00:47:17 -0500

    main navigation

--- src/components/layout/MainNavigation.svelte
+++ src/components/layout/MainNavigation.svelte
@@ -1,15 +1,13 @@
 <script>
-  export let app
+  export let app = ''
 </script>
 
 <div class="nav">
-  <a href="/admin-control-panel" class="btn-floating btn-large waves-effect waves-light grey"><i class="material-icons">security</i></a>
-  <a href="/client-area" class="btn-floating btn-large waves-effect waves-light grey"><i class="material-icons">card_membership</i></a>
-  <a href="/webmaster" class="btn-floating btn-large waves-effect waves-light grey"><i class="material-icons">store</i></a>
-  <a href="/my-account" class="btn-floating btn-large waves-effect waves-light grey my-account"><i class="material-icons">lock</i></a>
+  <a href="/admin-control-panel" class={`btn-floating btn-large waves-effect waves-light ${app === 'admin-control-panel' ? 'blue lighten-2' : 'grey'}`}><i class="material-icons">security</i></a>
+  <a href="/client-area" class={`btn-floating btn-large waves-effect waves-light ${app === 'client-area' ? 'blue lighten-2' : 'grey'}`}><i class="material-icons">card_membership</i></a>
+  <a href="/webmaster" class={`btn-floating btn-large waves-effect waves-light ${app === 'webmaster' ? 'blue lighten-2' : 'grey'}`}><i class="material-icons">store</i></a>
+  <a href="/my-account" class={`my-account btn-floating btn-large waves-effect waves-light ${app === 'my-account' ? 'blue lighten-2' : 'grey'}`}><i class="material-icons">lock</i></a>
 </div>
-
-
 
 <style>
   .nav {
