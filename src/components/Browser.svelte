9a9a9ecbafc216fa21982f053eea9546a1e6b138
istrav <travis.burandt@gmail.com>
2022-05-14 02:28:04 -0500

    license key

--- src/components/Browser.svelte
+++ src/components/Browser.svelte
@@ -44,10 +44,8 @@
   <br />
   <br />
   <div class="container">
-
     <div style="max-width: 500px; margin: 0 auto;">
       <h3 class="demo-title">Bulletin Board System</h3>
-      <h3 class="demo-description">Powered by a PostgreSQL database with a running NestJS backend, Svelte frontend, and NativeScript mobile app.</h3>
       <p class="demo-detail">(notice: /community_folder/demo/ resets every 1 hour.)</p>
     </div>
     <a class="waves-effect waves-light btn white black-text">home</a>
@@ -64,6 +62,86 @@
     <a class="waves-effect waves-light btn grey">events</a>
     <a class="waves-effect waves-light btn grey">calendar</a>
     <a class="waves-effect waves-light btn grey">results</a>
+  </div>
+  <br />
+  <br />
+  <div style="max-width: 700px; margin: 0 auto;">
+    <h3 class="demo-title">License Key</h3>
+    <h3 class="demo-description">Application runs "self hosted" as in on your own internet servers (enterprise) or in the cloud (small-large).</h3>
+    <br />
+    <table class="centered" style="background: #fff; color: #111;">
+      <thead>
+        <tr>
+            <th></th>
+            <th>Trial</th>
+            <th>Small</th>
+            <th>Medium</th>
+            <th>Large</th>
+            <th>Enterprise</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Websites</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>∞</td>
+        </tr>
+        <tr>
+          <td>Registered Users</td>
+          <td>1,000</td>
+          <td>10,000</td>
+          <td>100,000</td>
+          <td>500,000</td>
+          <td>∞</td>
+        </tr>
+        <tr>
+          <td>Online Users</td>
+          <td>25</td>
+          <td>100</td>
+          <td>1,000</td>
+          <td>5,000</td>
+          <td>∞</td>
+        </tr>
+        <tr>
+          <td>Media Storage</td>
+          <td>25 GB</td>
+          <td>500 GB</td>
+          <td>2 TB</td>
+          <td>5 TB</td>
+          <td>∞</td>
+        </tr>
+        <tr>
+          <td>Support</td>
+          <td>✖</td>
+          <td>✓</td>
+          <td>✓</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><a href="/client-area" class="btn btn-small blue lighten-2 black-text">FREE</a></td>
+          <td><a href="/client-area" class="btn btn-small blue lighten-2 black-text">$99</a></td>
+          <td><a href="/client-area" class="btn btn-small blue lighten-2 black-text">$299</a></td>
+          <td><a href="/client-area" class="btn btn-small blue lighten-2 black-text">$599</a></td>
+          <td><a href="/client-area" class="btn btn-small blue lighten-2 black-text">Contact Us</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <br />
+  <br />
+  <div style="max-width: 700px; margin: 0 auto;">
+    <h3 class="demo-title">Source Code</h3>
+    <h3 class="demo-description">Application is built with a NestJS backend, Svelte frontend, and NativeScript mobile app.</h3>
+    <br />
+    <div style="text-align: center;">
+      <a href="/client-area" class="btn btn-large red lighten-2 black-text">readme.md</a>
+    </div>
   </div>
   <br />
   <br />
@@ -109,6 +187,7 @@
 
   .demo-detail {
     font-size: 1.2em;
+    margin: 0 0 1em;
   }
 
   /* The browser window */
