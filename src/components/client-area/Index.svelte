483b5709fe9c68a03fd4017fb2610281713dbd3e
istrav <travis.burandt@gmail.com>
2022-05-15 22:07:11 -0500

    client area index

--- src/components/client-area/Index.svelte
+++ src/components/client-area/Index.svelte
@@ -10,10 +10,43 @@
   })
 </script>
 
-{#if account}
-  hello world {account.email}
-{/if}
-
-{#if data}
+<!-- {#if data}
   {JSON.stringify(data)}
-{/if}
+{/if} -->
+
+<div class="wrapper">
+  <a href={`/client-area/clients/create`} class="btn right black lighten-2">CREATE</a>
+  <h4>Clients</h4>
+  <div class="card">
+    <table class="centered">
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th style="text-align: right;">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#if data}
+          {#each data as record}
+            <tr>
+              <td>{record.username}</td>
+              <td style="text-align: right;">
+                <a href={`/client-area/clients/update/${record.id}`} class="btn btn-small grey lighten-2 black-text">Update</a>
+                <a href={`/client-area/clients/delete/${record.id}`} class="btn btn-small grey lighten-2 black-text">Delete</a>
+              </td>
+            </tr>
+          {/each}
+        {/if}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<style>
+  h3 {
+    margin: 0;
+  }
+  .wrapper {
+    margin: 1em;
+  }
+</style>
