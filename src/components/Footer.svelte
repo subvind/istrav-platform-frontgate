4c437664b2d0027a8f69f6b8ebcbb603cced9220
istrav <travis.burandt@gmail.com>
2022-08-06 17:59:39 -0500

    stream

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,53 +1,26 @@
-<footer class="page-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Fair Source 25 (v0.9).</h5>
-        <p class="grey-text text-lighten-4">Not open source. Not closed source. The Fair Source License allows everyone to see the source code and makes the software free to use for a limited number of users in your organization. It offers some of the benefits of open source while preserving the ability to charge for the software. <a href="https://fair.io" target="_blank" style="color: #111;">https://fair.io</a></p>
-        <a href="https://istrav.com/about" class="btn white black-text">ABOUT</a>
-        <br />
-        <br />
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text">/istrav/</h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="//istrav.com/production">Production</a></li>
-          <li><a class="grey-text text-lighten-3" href="//istrav.com/solutions">Solutions</a></li>
-          <li><a class="grey-text text-lighten-3" href="//istrav.com/apps">Apps</a></li>
-          <li><a class="grey-text text-lighten-3" href="//istrav.com/platforms">Platforms</a></li>
-          <li><a class="grey-text text-lighten-3" href="//istrav.com/projects">Projects</a></li>
+<footer>
+  <nav class="white black-text">
+    <div class="container">
+      <div class="nav-wrapper">
+        <ul class="">
+          <li class=""><a href="/" target="_blank">/istrav/ © {new Date().getFullYear()}.</a></li>
+          <li class=""><a href="https://istrav.com" target="_blank">Made with &hearts; by istrav.com.</a></li>
+        </ul>
+        <ul class="right">
+          <li><a href={`https://istrav.com`}>Website</a></li>
+          <li><a href={`https://stream.istrav.dev`}>Stream</a></li>
+          <li><a href={`https://istrav.dev`}>Headquarters</a></li>
         </ul>
       </div>
     </div>
-  </div>
-  <div class="footer-copyright">
-    <div class="container">
-
-    <span>
-      <a href="https://istrav.net" class="copyright">/istrav/</a> © {new Date().getFullYear()}. Made with <span>♥</span> by <a href="https://istrav.com" class="white-text">istrav.com</a>.
-    </span>
-    <a class="grey-text text-lighten-4 right" href="/terms-and-conditions">Terms & Conditions</a>
-    <span class="right" style="padding: 0 1em;"> • </span>
-    <a class="grey-text text-lighten-4 right" href="/privacy-policy">Privacy Policy</a>
-    </div>
-  </div>
+  </nav>
 </footer>
 
 <style>
-  .page-footer {
-    box-shadow: 0 0 1em #000;
+  footer {
+    box-shadow: 0 0 5em #aaa;
   }
-  .copyright {
-    color: #fff;
-  }
-  ul {
-    /* text-align: center; */
-  }
-
-  ul li {
-    margin: 0.5em 0 0.5em 0;
-  }
-  ul li a {
+  footer ul li a {
     color: #111;
   }
 </style>
